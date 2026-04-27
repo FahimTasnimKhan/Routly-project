49f3107e457b186e547344ea109a879607d51438
FahimTasnimKhan <fahimtasnimkhan@gmail.com>
2026-04-27 10:47:48 +0600

    Updated the index.html files

--- login.php
+++ login.php
@@ -4,12 +4,12 @@ require_once('DBconnect.php');
 if(isset($_POST['username']) && isset($_POST['password'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $sql = "SELECT * FROM users WHERE passenger_id = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result) > 0){
         // echo "Let him enter";
-        header("Location: home.php");
+        header("Location: home.html");
     }
     else{
         echo "Wrong username or password";
